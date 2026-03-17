f9bffa9e34b2be9d0df08d22a8abcc2b2976f770
KamilK <kashaev@gmail.com>
2026-03-17 15:59:26 +0500

    fix ui

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-figma.showUI(__html__, { width: 640, height: 480 });
+figma.showUI(__html__, { width: 640, height: 420 });
 
 // Функция для капитализации первой буквы строки
 function capitalizeFirstLetter(str) {
